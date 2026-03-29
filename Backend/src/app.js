d73b9a64a50bcd4e318af7203696b5a1f371b860
Lakshyawardhansinghrathore <lakshyarathore005@gmail.com>
2026-03-29 23:57:03 +0530

    Adding cors package so that it can integrate with the frontend

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -2,8 +2,10 @@ const express = require('express');
 const multer = require('multer');
 const uploadFile = require("./services/storage.services")
 const postModel = require("./models/post.model")
+const cors = require('cors')
 
 const app = express();
+app.use(cors()) // This will allow us to access our backend api from frontend which is running on different port
 app.use(express.json()); // this middleware can handle raw data format but out form data format
 // install -> npm i multer
 
